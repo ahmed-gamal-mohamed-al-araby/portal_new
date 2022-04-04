3d72792f2413de76b73f965c2e1c58879d085df4
Mahmoud Ahmed <mahmoud.ahmed@eec.local>
2022-04-04 12:13:24 +0200

    update user role index

--- resources/views/dashboard-views/user_role/edit.blade.php
+++ resources/views/dashboard-views/user_role/edit.blade.php
@@ -3,244 +3,197 @@ $currentLanguage = app()->getLocale();
 @endphp
 
 @extends('dashboard-views.layouts.master', [
-'parent' => 'department',
-'child' => 'edit',
+'parent' => 'user_roles',
+'child' => 'Edit',
 ])
 
 {{-- Custom Title --}}
 @section('title')
-    @lang('site.Edit') @lang('site.the_department')
+@lang('site.Edit') @lang('site.user_role')
 @endsection
 
 {{-- Custom Styles --}}
 @section('styles')
-    {{-- select 2 --}}
-    <link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }}">
+{{-- select 2 --}}
+<link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }}">
 @endsection
 
 {{-- Page content --}}
 @section('content')
 
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-6">
-                    <h1>@lang('site.Departments')</h1>
-                </div>
-                <div class="col-md-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('home') }}"> @lang('site.Home')</a></li>
-                        <li class="breadcrumb-item"><a href="{{ route('department.index') }}">
-                                @lang('site.Departments')</a>
-                        </li>
-                        <li class="breadcrumb-item active"> @lang('site.Edit') @lang('site.department')</li>
-                    </ol>
-                </div>
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <h1>@lang('site.user_roles')</h1>
+            </div>
+            <div class="col-md-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{ route('home') }}"> @lang('site.Home')</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('department.index') }}">
+                            @lang('site.user_roles')</a>
+                    </li>
+                    <li class="breadcrumb-item active"> @lang('site.Edit') @lang('site.user_role')</li>
+                </ol>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <section class="content service-content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="form-layout">
-                        {{-- Form to edit department --}}
-                        <form action="{{ route('department.update', $department->id) }}" method="POST"
-                            id="editDepartment">
-                            @csrf
-                            @method('put')
-                            <div class="row">
+<section class="content service-content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="form-layout">
+                    {{-- Form to create department --}}
+                    <form action="{{ route('user-roles.update', $username_id) }}" method="POST" id="">
+                        @csrf
+                        @method('put')
 
-                                {{-- Arabic name --}}
-                                <div class="col-md-6 col-12">
-                                    <div class="form-group">
-                                        <label for="Add Service ">@lang('site.arabic_name') </label>
-                                        <input type="text" name="name_ar" value="{{ $department->name_ar }}"
-                                            class="form-control" placeholder="@lang('site.arabic_name')">
-                                    </div>
-                                    @error('name_ar')
-                                        <p class="text-bold text-danger mt-n3 font-n8rem">{{ $message }}</p>
-                                    @enderror
+                        <div class="row">
+
+                            {{-- Choose user --}}
+                            <div class="col-md-6 col-12">
+                                <div class="form-group ">
+                                    <label> @lang('site.Choose') @lang('site.user') </label>
+                                    <select id='user' name="user_id" class="form-control users" style="width: 100%;">
+                                        <option> </option>
+                                        @foreach ($users as $user)
+                                        <option value="{{ $user->id }} " @if($username_id == $user->id) selected @endif>
+                                            {{ $user->name_ar }}
+                                        </option>
+                                        @endforeach
+                                    </select>
                                 </div>
-
-                                {{-- English name --}}
-                                <div class="col-md-6 col-12">
-                                    <div class="form-group">
-                                        <label for="Add Service ">@lang('site.english_name') </label>
-                                        <input type="text" name="name_en" value="{{ $department->name_en }}"
-                                            class="form-control" placeholder="@lang('site.english_name')">
-                                    </div>
-                                    @error('name_en')
-                                        <p class="text-bold text-danger mt-n3 font-n8rem">{{ $message }}</p>
-                                    @enderror
-                                </div>
-
-                                {{-- Choose sector --}}
-                                <div class="col-md-6 col-12">
-                                    <div class="form-group ">
-                                        <label> @lang('site.Choose') @lang('site.sector') </label>
-                                        <select id='sector' name="sector_id" class="form-control" style="width: 100%;">
-                                            <option> </option>
-                                            @foreach ($sectors as $sector)
-                                                <option value="{{ $sector->id }}"
-                                                    {{ $department->sector->id == $sector->id ? 'selected' : '' }}>
-                                                    {{ $sector['name_' . $currentLanguage] }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    @error('sector_id')
-                                        <p class="text-bold text-danger mt-n3 font-n8rem">{{ $message }}</p>
-                                    @enderror
-                                </div>
-
-                                {{-- Choose manager --}}
-                                <div class="col-md-6 col-12">
-                                    <div class="form-group ">
-                                        <label> @lang('site.Choose') @lang('site.manager') </label>
-                                        <select id='manager' name="manager_id" class="form-control" style="width: 100%;">
-                                            <option> </option>
-                                            @foreach ($users as $user)
-                                                <option value="{{ $user->id }}"
-                                                    {{ $department->manager->id == $user->id ? 'selected' : '' }}>
-                                                    {{ $user['name_' . $currentLanguage] }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    @error('manager_id')
-                                        <p class="text-bold text-danger mt-n3 font-n8rem">{{ $message }}</p>
-                                    @enderror
-                                </div>
-
-                                {{-- Choose delegated --}}
-                                <div class="col-md-6 col-12">
-                                    <div class="form-group ">
-                                        <label> @lang('site.Choose') @lang('site.manager_delegate') </label>
-                                        <select id='delegated' name="delegated_id" class="form-control"
-                                            style="width: 100%;">
-                                            <option> </option>
-                                            @foreach ($users as $user)
-                                                <option value="{{ $user->id }}"
-                                                    {{ $department->delegated->id == $user->id ? 'selected' : '' }}>
-                                                    {{ $user['name_' . $currentLanguage] }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    @error('delegated_id')
-                                        <p class="text-bold text-danger mt-n3 font-n8rem">{{ $message }}</p>
-                                    @enderror
-                                </div>
-
-                                {{-- Submit button --}}
-                                <div class="col-12">
-                                    <input type="submit" class="btn btn-success" value="@lang('site.Submit')">
-                                </div>
-
+                                @error('user_id')
+                                <p class="text-bold text-danger mt-n3 font-n8rem">{{ $message }}</p>
+                                @enderror
                             </div>
-                        </form>
-                    </div>
+
+                            {{-- Choose role --}}
+                            <div class="col-md-6 col-12">
+                                <div class="form-group ">
+                                    <label> @lang('site.Choose') @lang('site.role') </label>
+                                    <select id='role' name="role_id[]" class="form-control roles" multiple style="width: 100%;">
+                                        <option> </option>
+                                        @foreach ($roles as $role)
+                                        <option value="{{ $role->id }}" @if(in_array($role->id, $roles_select)) selected @endif>
+                                            {{ $role->name }}
+                                        </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                @error('role_id')
+                                <p class="text-bold text-danger mt-n3 font-n8rem">{{ $message }}</p>
+                                @enderror
+                            </div>
+
+
+                            {{-- Submit button --}}
+                            <div class="col-12">
+                                <input type="submit" class="btn btn-success" value="@lang('site.Submit')">
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
 @endsection
 
 {{-- Custom scripts --}}
 @section('scripts')
-    {{-- select 2 --}}
-    <script src="{{ asset('plugins/select2/js/select2.min.js') }}"></script>
-    <script src="{{ asset('plugins/jquery-validation/jquery.validate.min.js') }}"></script>
-    <script src="{{ asset('plugins/jquery-validation/additional-methods.min.js') }}"></script>
-    <script>
-        /*  Start defining select2  */
-        $('#sector').select2({
-            placeholder: "@lang('site.Choose') @lang('site.sector')",
-            allowClear: true,
-        });
-        $('#manager').select2({
-            placeholder: "@lang('site.Choose') @lang('site.manager')",
-            allowClear: true,
-        });
-        $('#delegated').select2({
-            placeholder: "@lang('site.Choose') @lang('site.manager_delegate')",
-            allowClear: true,
-        });
-        /*  End defining select2  */
+{{-- select 2 --}}
+<script src="{{ asset('plugins/select2/js/select2.min.js') }}"></script>
+<script src="{{ asset('plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('plugins/jquery-validation/additional-methods.min.js') }}"></script>
+<script>
+    /*  define select2  */
+    $('#user').select2({
+        placeholder: "@lang('site.Choose') @lang('site.user')",
+        allowClear: true,
+    });
+    $('#role').select2({
+        placeholder: "@lang('site.Choose') @lang('site.manager')",
+        allowClear: true,
+    });
+    $('#delegated').select2({
+        placeholder: "@lang('site.Choose') @lang('site.manager_delegate')",
+        allowClear: true,
+    });
+    /*  End defining select2  */
 
-        /*  Start validation  */
-        $(function() {
-            $('#editDepartment').validate({
-                rules: {
-                    name_ar: {
-                        required: true,
-                        minlength: 2,
-                        maxlength: 255
-                    },
-                    name_en: {
-                        required: true,
-                        minlength: 2,
-                        maxlength: 255
-                    },
-                    sector_id: {
-                        required: true,
-                    },
-                    manager_id: {
-                        required: true,
-                    },
-                    delegated_id: {
-                        required: true,
-                    }
+    /*  Start validation  */
+    $(function() {
+        $('#createDepartment').validate({
+            rules: {
+                name_ar: {
+                    required: true,
+                    minlength: 2,
+                    maxlength: 255
                 },
-                messages: {
-                    name_ar: {
-                        required: " @lang('site.Name') @lang('site.is_required')",
-                        minlength: "@lang('site.minlength_letters_number') 2",
-                        maxlength: "@lang('site.maxlength_letters_number') 255",
-
-                    },
-                    name_en: {
-                        required: " @lang('site.Name') @lang('site.is_required')",
-                        minlength: "@lang('site.minlength_letters_number') 2",
-                        maxlength: "@lang('site.maxlength_letters_number') 255",
-                    },
-                    sector_id: {
-                        required: " @lang('site.the_sector') @lang('site.is_required')",
-                    },
-                    manager_id: {
-                        required: " @lang('site.the_manager') @lang('site.is_required')",
-                    },
-                    delegated_id: {
-                        required: " @lang('site.manager_delegate') @lang('site.is_required')",
-                    },
+                name_en: {
+                    required: true,
+                    minlength: 2,
+                    maxlength: 255
                 },
-                errorElement: 'span',
-                errorPlacement: function(error, element) {
-                    error.addClass('invalid-feedback');
-                    element.closest('.form-group').append(error);
+                sector_id: {
+                    required: true,
                 },
-                highlight: function(element, errorClass, validClass) {
-                    $(element).addClass('is-invalid');
+                manager_id: {
+                    required: true,
                 },
-                unhighlight: function(element, errorClass, validClass) {
-                    $(element).removeClass('is-invalid');
+                delegated_id: {
+                    required: true,
                 }
-            });
+            },
+            messages: {
+                name_ar: {
+                    required: " @lang('site.Name') @lang('site.is_required')",
+                    minlength: "@lang('site.minlength_letters_number') 2",
+                    maxlength: "@lang('site.maxlength_letters_number') 255",
 
-            $('#manager').on('change', function() {
-                $('#delegated').find('option').prop('disabled', false);
-                $('#delegated').find(`option[value="${$(this).val()}"]`).prop('disabled', true);
-            })
-
-            $('#delegated').on('change', function() {
-                $('#manager').find('option').prop('disabled', false);
-                $('#manager').find(`option[value="${$(this).val()}"]`).prop('disabled', true);
-            })
-
-            $('#delegated').find(`option[value="${$('#manager').val()}"]`).prop('disabled', true);
-            $('#manager').find(`option[value="${$('#delegated').val()}"]`).prop('disabled', true);
-            
+                },
+                name_en: {
+                    required: " @lang('site.Name') @lang('site.is_required')",
+                    minlength: "@lang('site.minlength_letters_number') 2",
+                    maxlength: "@lang('site.maxlength_letters_number') 255",
+                },
+                sector_id: {
+                    required: " @lang('site.the_sector') @lang('site.is_required')",
+                },
+                manager_id: {
+                    required: " @lang('site.the_manager') @lang('site.is_required')",
+                },
+                delegated_id: {
+                    required: " @lang('site.manager_delegate') @lang('site.is_required')",
+                },
+            },
+            errorElement: 'span',
+            errorPlacement: function(error, element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight: function(element, errorClass, validClass) {
+                $(element).addClass('is-invalid');
+            },
+            unhighlight: function(element, errorClass, validClass) {
+                $(element).removeClass('is-invalid');
+            }
         });
-        /*  End validation  */
-    </script>
+
+        $('#manager').on('change', function() {
+            $('#delegated').find('option').prop('disabled', false);
+            $('#delegated').find(`option[value="${$(this).val()}"]`).prop('disabled', true);
+        })
+
+        $('#delegated').on('change', function() {
+            $('#manager').find('option').prop('disabled', false);
+            $('#manager').find(`option[value="${$(this).val()}"]`).prop('disabled', true);
+        })
+    });
+    /*  End validation  */
+</script>
 @endsection
